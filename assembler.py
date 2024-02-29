9cbc85d2344b110742e3c2436d2ebdd496b003bd
Daniele Perot <daniele.perot@nordhealth.com>
2024-02-29 19:08:38 +0200

    Improve arguments parsing

--- assembler.py
+++ assembler.py
@@ -1,8 +1,31 @@
-import sys
+from argparse import ArgumentParser
 
 from compiler import Program
 from linker import Linker
 from output import print_output
+
+
+def arguments():
+    parser = ArgumentParser(
+        prog="Danmos Assembler",
+        description=" A simple, basic, uncompleted 6502 assembler",
+    )
+
+    parser.add_argument('filename')
+    parser.add_argument(
+        '-s',
+        '--start-position',
+        help="Start position of the program (hexidecimal) [default: 0xC000]",
+        default="C000"
+    )
+    parser.add_argument(
+        '-b',
+        '--bytes-per-line',
+        help="Number of bytes per line in instructions output [default: 8]",
+        default="8"
+    )
+
+    return parser.parse_args()
 
 
 def parse_file(file) -> Program:
@@ -19,13 +42,19 @@ def parse_file(file) -> Program:
 
 
 if __name__ == '__main__':
-    assert len(sys.argv) > 1, "No file provided. Please provide a file path."
+    arguments = arguments()
 
-    program = parse_file(sys.argv[1])
+    program = parse_file(arguments.filename)
     labels = program.labels()
     bytecode = program.bytecode()
 
-    start_pos = 0xC000
+    start_pos = int(arguments.start_position, 16)
+
     bytecode = Linker(start_pos).parse(labels, bytecode)
 
-    print_output(labels, bytecode, start_pos, bytes_per_line=8)
+    print_output(
+        labels,
+        bytecode,
+        start_pos,
+        bytes_per_line=int(arguments.bytes_per_line)
+    )
